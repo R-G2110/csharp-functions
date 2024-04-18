8a4c67873eb91075a50df5b81580e70f68f49216
R-G2110 <argeldelacruz86@gmail.com>
2024-04-18 14:33:11 +0200

    [Add]: ElevaArrayAlQuadrato function

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -24,13 +24,37 @@
                 Console.Write(")");
             }
 
-            
+            //Funzione che eleva al quadrato un numero
+            int Quadrato(int num)
+            {
+                return num * num;
+            }
+
+            //Funzione che eleva al quadrato l'array
+            int[] ElevaArrayAlQuadrato(int[] array)
+            {
+                int[] newArray = new int[array.Length];
+                for (int i = 0; i < array.Length; i++)
+                {
+                    newArray[i] = Quadrato(array[i]);
+                }
+                return newArray;
+            }
+
 
             //
 
             //Dati di prova
             int[] arrayNumeri = { 2, 6, 7, 5, 3, 9 };
+
+            // Stampa l'array di numeri
+            Console.WriteLine("Stampa dell'array originale:");
             StampaArray(arrayNumeri);
+
+            // Stampa l'array dove ogni numero è stato elevato al quadrato
+            Console.WriteLine("Array con elementi elevati al quadrato:");
+            int[] arrQuadrato = ElevaArrayAlQuadrato(arrayNumeri);
+            StampaArray(arrQuadrato);
         }
     }
 }
