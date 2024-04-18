c92c5d776200323036ea85dbc305e363572723f4
R-G2110 <argeldelacruz86@gmail.com>
2024-04-18 14:22:43 +0200

    [Add]: StampaVirgolaESpazio function

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -5,16 +5,32 @@
         static void Main(string[] args)
         {
             //FUNCTIONS
+            //Funzione che stampa virgola e spazio
+            void StampaVirgolaESpazio()
+            {
+                Console.Write(", ");
+            }
+
             //Funzione che stampa l'array
             void StampaArray(int[] array)
             {
+                Console.Write("(");
                 for (int i = 0; i < array.Length; i++)
                 {
-                    Console.Write($"[{array[i]}]");
+                    Console.Write(array[i]);
+                    if (i < array.Length - 1)
+                        StampaVirgolaESpazio();
                 }
-                Console.WriteLine();
+                Console.Write(")");
             }
 
+            
+
+            //
+
+            //Dati di prova
+            int[] arrayNumeri = { 2, 6, 7, 5, 3, 9 };
+            StampaArray(arrayNumeri);
         }
     }
 }
