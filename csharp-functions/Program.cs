088d2b85569a6cba9f8e481dd937776bdee7cc39
R-G2110 <argeldelacruz86@gmail.com>
2024-04-18 14:02:54 +0200

    [Add]: StampaArray function

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //FUNCTIONS
+            //Funzione che stampa l'array
+            void StampaArray(int[] array)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write($"[{array[i]}]");
+                }
+                Console.WriteLine();
+            }
+
         }
     }
 }
